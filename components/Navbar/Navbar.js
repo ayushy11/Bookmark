2c53bdb7627d8929087fbe03aa3b031896d96bc8
AYUSH YADAV <ayushexx@gmail.com>
2021-08-03 21:18:59 +0530

    style: navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,10 +1,24 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image, Text, Button } from "@chakra-ui/react";
 import React from "react";
 
 function Navbar() {
   return (
-    <Flex w="100%" p="5" bg="lightblue">
-      BOOKMARK
+    <Flex w="100%" p="3rem" bg="lightblue">
+      <Flex w="50%">
+        <Image src="/images/logo-bookmark.svg" pl="8rem" h="1.875rem" />
+      </Flex>
+      <Flex w="50%" justifyContent="center" alignItems="center">
+        <a>
+          <Text pr="3rem">FEATURES</Text>
+        </a>
+        <a>
+          <Text pr="3rem">PRICING</Text>
+        </a>
+        <a>
+          <Text pr="3rem">CONTACT</Text>
+        </a>
+        <Button>LOGIN</Button>
+      </Flex>
     </Flex>
   );
 }
